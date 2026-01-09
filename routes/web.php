3604a97a76ae92d64ed9496ce612591e77a975e1
Didinnn21 <d6258113@gmail.com>
2026-01-09 22:10:16 +0700

    update public

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dahsboard', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
 });
